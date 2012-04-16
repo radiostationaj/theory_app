890d930bf275f0c8c3b2b8e55b40add7615296af
Your Name <ajslivinill@hotmail.com>
2012-04-16 00:17:47 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheoryApp::Application.config.secret_token = 'c87735ce1507c2537dfb191cd99a55d436eaa044c8f653d6a1f67593a553bf938edf2d7f97f2a5d4388bf20aea7cb9dafd95b1b2620a83c93ceed1ac7434d725'
